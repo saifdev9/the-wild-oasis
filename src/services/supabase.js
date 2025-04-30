31b7f4243a02e037d699e70ba13a56544be89714
saifdev9 <sa9825340@gmail.com>
2025-04-30 21:07:43 +0530

    first

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://qhmcbnhursnsvbuimytt.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFobWNibmh1cnNuc3ZidWlteXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcwMTA2MzIsImV4cCI6MjA1MjU4NjYzMn0.IakxncgFQK756aLZREwODRetxWPyyUg96uelPT7amTU";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
